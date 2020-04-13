79cf2860c4af32a21f875d3c8301d23256cde32d
Paehel Shah <shahpaehel@gmail.com>
2020-04-13 18:18:27 +0530

    done

--- js/Log.js
+++
@@ -1,8 +0,0 @@
-class Log extends BaseClass{
-  constructor(x,y,width,height,angle){
-    super(x,y,width,height,angle);
-    this.image = loadImage("sprites/wood2.png");
-    Matter.Body.setAngle(this.body, angle);
-  }
-
-}
